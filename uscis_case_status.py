8e3ddd8122810ca7bd3f348baee8e4cf16be82b0
Anderson Mesquita <andersonvom@gmail.com>
2017-09-22 20:05:15 +0200

    Add action date check to case description
    This will also check for updates in the date of the last action
    performed in a particular case. It should help with cases where the
    status doesn't change even though a new action was taken by the USCIS.

--- uscis_case_status.py
+++ uscis_case_status.py
@@ -6,6 +6,7 @@ import urllib.request
 
 app_receipt_num=sys.argv[1]
 current_status=sys.argv[2]
+current_action_date=sys.argv[3]
 
 url = 'https://egov.uscis.gov/casestatus/mycasestatus.do'
 data = 'appReceiptNum=%s' % app_receipt_num
@@ -14,8 +15,11 @@ with urllib.request.urlopen(url, data=bytes(data, 'utf-8')) as res:
     page = str(res.read())
 
 single_line_page = page.replace('\\r\\n', ' ').replace('\\t', ' ')
+
 match = re.search('<h1>(.*?)</h1>', single_line_page)
 new_status = match.group(1)
+match = re.search('On (\w+ \d+, \d+),', single_line_page)
+new_action_date = match.group(1)
 
-if new_status != current_status:
-    print('%s: %s' % (app_receipt_num, new_status), end='')
+if new_status != current_status or new_action_date != current_action_date:
+    print('%s: %s - %s' % (app_receipt_num, new_status, new_action_date), end='')
