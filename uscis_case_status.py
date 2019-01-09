ba77353ad81e8a4fd7f013c92f3ef7520d2a7cb0
Anderson Mesquita <andersonvom@gmail.com>
2019-01-08 22:54:28 -0500

    Handle too many requests scenario
    If the script is run too many times in a short time window, the USCIS
    may block the IP address from making new requests. This will at least
    let the user know if that is ever the case.

--- uscis_case_status.py
+++ uscis_case_status.py
@@ -16,6 +16,11 @@ with urllib.request.urlopen(url, data=bytes(data, 'utf-8')) as res:
 
 single_line_page = page.replace('\\r\\n', ' ').replace('\\t', ' ')
 
+match = re.search('has been locked out', single_line_page)
+if match is not None:
+    print('[ERROR] Too many requests')
+    sys.exit(1)
+
 match = re.search('<h1>(.*?)</h1>', single_line_page)
 new_status = match.group(1)
 match = re.search('On (\w+ \d+, \d+),', single_line_page)
